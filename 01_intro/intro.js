11da4388ee85248dbd85be33b6f6c52c3308ab12
NikRuchkin1 <NikRuchkin1>
2021-11-09 00:48:27 +0300

    Added test

---
+++ 01_intro/intro.js
@@ -0,0 +1,16 @@
+function expect(value) {
+  return {
+    toBe: (exp) => {
+      if (value === epx) {
+        console.log('Success');
+      } else {
+        console.error(`Value is ${value}, but expectation is ${exp}`);
+      }
+    },
+  };
+}
+const sum = (a, b) => a + b;
+
+const nativeNull = () => null;
+
+module.exports = { sum, nativeNull };
